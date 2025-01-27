54e6abb886822ef276fe4adebd4819d84fcc3c1b
Mikicrep <mikicrepoffical@gmail.com>
2025-01-27 21:20:30 +0100

    Remove cancer

--- source/logic/clearmap.cpp
+++ source/logic/clearmap.cpp
@@ -1,7 +1,7 @@
 #include "data.h"
 
 namespace logic {
-    void ClearMap(data::Game& game) {
+    void ClearMap(data::Game &game) {
         for(int x = 0; x < 8; x++) {
             for(int y = 0; y < 2; y++) {
                 game.map[x][y].unrot = 0;
