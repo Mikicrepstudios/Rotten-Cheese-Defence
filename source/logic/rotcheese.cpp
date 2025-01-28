cd7ee1c3d64a5ecbdaa5e96581eb382e5e24c3fb
Mikicrep <mikicrepoffical@gmail.com>
2025-01-28 22:57:08 +0100

    Cheese disappearing

--- source/logic/rotcheese.cpp
+++ source/logic/rotcheese.cpp
@@ -10,10 +10,7 @@ namespace logic {
         int x = logic::GenRanNum(0, 7);
         int y = logic::GenRanNum(0, 1);
 
-        if(game.map[x][y].state != 3)
+        if(game.map[x][y].state != 4)
             game.map[x][y].state += 1;
-
-        //if(game.map[x][y].state == 3)
-            // Add timer here to remove cheese
     }
 }
