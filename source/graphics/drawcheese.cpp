b4a693832627e0887eb865f7c5bfa60187199abb
Mikicrep <mikicrepoffical@gmail.com>
2025-01-29 23:21:59 +0100

    Game is now fully resizeable

--- source/graphics/drawcheese.cpp
+++ source/graphics/drawcheese.cpp
@@ -1,5 +1,6 @@
 #include "mf/core.h"
 #include "mf/colors.h"
+#include "mf/extra.h"
 #include "mf/graphics.h"
 
 #include "assets.h"
@@ -9,7 +10,11 @@ namespace graphics {
     void DrawCheese(core::MF_Window window, data::Game &game, assets::Cheese cheeseAssets) {
         for(int x = 0; x < 8; x++) {
             for(int y = 0; y < 2; y++) {
-                SDL_Rect cheeseRect = {(50 * (x + 1)) + (x * 100), (60 * (y + 1)) + (y * 100), 100, 100};
+                // Variables used for scaling elements
+                float wFactor = extra::castToFloat(window.width) / 1280;
+                float hFactor = extra::castToFloat(window.height) / 800;
+
+                SDL_Rect cheeseRect = {extra::castToInt(((50 * (x + 1)) + (x * 100)) *  wFactor), extra::castToInt(((60 * (y + 1)) + (y * 100)) * hFactor), extra::castToInt(100 * wFactor), extra::castToInt(100 * hFactor)};
                 // Draw cheese
                 switch(game.map[x][y].state) {
                     case 0:
--- source/logic/unrotcheese.cpp
+++ source/logic/unrotcheese.cpp
@@ -1,4 +1,5 @@
 #include "mf/core.h"
+#include "mf/extra.h"
 #include "mf/logic.h"
 
 #include "data.h"
@@ -7,7 +8,11 @@ namespace logic {
     void UnRotCheese(core::MF_Window &window, data::Game &game) {
         for(int x = 0; x < 8; x++) {
             for(int y = 0; y < 2; y++) {
-                SDL_Rect cheeseRect = {(50 * (x + 1)) + (x * 100), (60 * (y + 1)) + (y * 100), 100, 100};
+                // Variables used for scaling elements
+                float wFactor = extra::castToFloat(window.width) / 1280;
+                float hFactor = extra::castToFloat(window.height) / 800;
+
+                SDL_Rect cheeseRect = {extra::castToInt(((50 * (x + 1)) + (x * 100)) *  wFactor), extra::castToInt(((60 * (y + 1)) + (y * 100)) * hFactor), extra::castToInt(100 * wFactor), extra::castToInt(100 * hFactor)};
                 
                 if(logic::IsMouseTouching(window.mouse.x, window.mouse.y, cheeseRect)) {
                     int originalState = game.map[x][y].state;
