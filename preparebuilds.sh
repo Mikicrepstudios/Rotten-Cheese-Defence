e061ed82078043e79db4d313de4e4067a6927a98
Mikicrep <mikicrepoffical@gmail.com>
2025-01-30 18:55:56 +0100

    Pack builds

--- preparebuilds.sh
+++ preparebuilds.sh
@@ -16,7 +16,7 @@ mkdir mfbuild
 cp output/app mfbuild
 cp output/mf mfbuild -r
 cp output/assets mfbuild -r
-cp output/*.md mfbuild
+#cp output/*.md mfbuild
 
 cd releases
 7z a linux.zip ../mfbuild
@@ -30,7 +30,7 @@ cp output/app-x86_64.exe mfbuild
 cp output/mf mfbuild -r
 cp output/assets mfbuild -r
 cp output/x86_64dll/* mfbuild
-cp output/*.md mfbuild
+#cp output/*.md mfbuild
 
 cd releases
 7z a win-x86_64.zip ../mfbuild
@@ -44,7 +44,7 @@ cp output/app-x86.exe mfbuild
 cp output/mf mfbuild -r
 cp output/assets mfbuild -r
 cp output/x86dll/* mfbuild
-cp output/*.md mfbuild
+#cp output/*.md mfbuild
 
 cd releases
 7z a win-x86.zip ../mfbuild
